ab54ac06051072bb2098c714b4d35ebf26fba841
Naruto-Takahashi <nalt@Nalt.localdomain>
2026-01-26 23:32:37 +0900

    fix(nvim): configure clangd separately with query-driver

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -20,20 +20,43 @@ return {
       -- Neovim 0.11+ の新しい作法 (Reverted to original working state)
       
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local servers = { "lua_ls", "pylsp", "clangd" }
+      local lspconfig = require("lspconfig")
+      local servers = { "lua_ls", "pylsp" } -- clangd は個別に設定するためここから外す
 
       for _, server in ipairs(servers) do
-        vim.lsp.enable(server)
-        
-        vim.api.nvim_create_autocmd("LspAttach", {
-          callback = function(args)
-            local client = vim.lsp.get_client_by_id(args.data.client_id)
-            if client then
-              client.capabilities = vim.tbl_deep_extend("force", client.capabilities, capabilities)
-            end
-          end,
+        -- vim.lsp.enable(server) -- Neovim 0.11+ (今回は lspconfig の setup を使う形に統一しても良いが、既存踏襲)
+        -- lspconfig を使う場合は通常 setup() を呼ぶのが定石
+        lspconfig[server].setup({
+            capabilities = capabilities
         })
       end
+      
+      -- clangd (C++) 個別設定
+      local clangd_cmd = { "clangd", "--background-index" }
+      local cxx_path = vim.fn.exepath("g++")
+      if cxx_path == "" then
+        cxx_path = vim.fn.exepath("clang++")
+      end
+
+      if cxx_path ~= "" then
+        -- コンパイラが見つかれば、そのパスだけでなく全てのバージョンを許可するパターンを追加
+        table.insert(clangd_cmd, "--query-driver=" .. cxx_path .. ",/usr/bin/g++*,/usr/bin/clang++*")
+      end
+
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+        cmd = clangd_cmd,
+      })
+
+      -- LspAttach の設定 (共通)
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client then
+            client.capabilities = vim.tbl_deep_extend("force", client.capabilities, capabilities)
+          end
+        end,
+      })
 
       -- キーマッピング (LSP関連)
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
