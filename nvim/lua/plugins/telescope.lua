c1376e6dc0526149bf19966b59436ce5d3355ff1
Naruto-Takahashi <nalt@Nalt.localdomain>
2026-01-31 04:16:19 +0900

    Update Neovim configuration

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,10 +2,17 @@ return {
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
 --      or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' },
+      dependencies = { 
+          'nvim-lua/plenary.nvim',
+          'nvim-telescope/telescope-ghq.nvim',
+      },
       config = function()
+        local telescope = require("telescope")
         local builtin = require("telescope.builtin")
         
+        -- 拡張機能の読み込み
+        telescope.load_extension('ghq')
+
         -- キーマッピングの設定
         -- <space>ff : ファイル名を検索
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
@@ -18,6 +25,9 @@ return {
         
         -- <space>fh : ヘルプタグを検索
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+        -- <space>fq : ghq管理のリポジトリを検索
+        vim.keymap.set('n', '<leader>fq', telescope.extensions.ghq.list, { desc = 'Telescope ghq list' })
       end
     }
 }
