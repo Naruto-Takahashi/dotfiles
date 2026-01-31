2aefe386d85a23ae86827d8459719b789db36fa5
Naruto-Takahashi <nalt@Nalt.localdomain>
2026-02-01 02:24:12 +0900

    nvim: pin treesitter to master branch for compatibility

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "master", -- Use the legacy branch for compatibility (configs module)
   build = ":TSUpdate",
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
