e22c3262bd34544d091cb0eb7770dc261ae8249a
Naruto-Takahashi <nalt@Nalt.localdomain>
2026-02-08 00:21:11 +0900

    style(wezterm): update tab colors (active: pastel red, inactive: soft gray)

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -55,12 +55,12 @@ local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
 local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local background = "#5c6d74"
+  local background = "#8e8e8e"
   local foreground = "#FFFFFF"
   local edge_background = "none"
 
   if tab.is_active then
-    background = "#ceab88"
+    background = "#a86c6c"
     foreground = "#FFFFFF"
   end
 
